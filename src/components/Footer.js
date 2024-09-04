ced9ec978dc101e59515488d570d60d76e05f82f
Keshav Gupta <guptakeshav0710@gmail.com>
2024-09-05 00:35:55 +0530

    home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -63,7 +63,7 @@ function Footer() {
   return (
     <footer className="flex flex-col w-full bg-black">
       {/* Top Section */}
-      <div className="flex flex-wrap gap-4 items-start px-4 py-6 w-full justify-between md:justify-center max-md:flex-col max-md:items-center">
+      <div className="flex flex-wrap gap-2 items-start px-2 py-6 w-full justify-between md:justify-center max-md:flex-col max-md:items-center">
         <EmailSubscription />
         {/* Footer Columns */}
         <div className="flex flex-1 gap-4 justify-between items-start font-medium flex-grow flex-wrap max-md:flex-col max-md:items-center">
