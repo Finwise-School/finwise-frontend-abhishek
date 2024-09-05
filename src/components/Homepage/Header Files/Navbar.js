3e11628bfd8db0fa7298c21f38cecaa5e4127653
abhisheksinghf <abhisheksinghfulanekar@gmail.com>
2024-09-05 14:01:24 +0530

    video added

--- src/components/Homepage/Header Files/Navbar.js
+++ src/components/Homepage/Header Files/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900" style={{marginTop: "-15px"}}>
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900" style={{ marginTop: "-15px" }}>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 relative">
         {/* Logo on the left */}
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={Logo} className="h-12" alt="Finwise School Logo" />
@@ -42,9 +42,66 @@ const Navbar = () => {
             />
           </svg>
         </button>
-        
+
+        {/* Mobile Menu Wrapper */}
+        <div
+          className={`fixed inset-0 bg-white dark:bg-gray-900 transition-transform transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden z-50`}
+          style={{ zIndex: 9999 }}
+        >
+          <div className="flex flex-col p-4">
+            <button
+              type="button"
+              className="self-end p-2 text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={handleToggle}
+            >
+              <span className="sr-only">Close menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1l12 12M13 1L1 13"
+                />
+              </svg>
+            </button>
+            <ul className="flex flex-col space-y-4 mt-4">
+              <li>
+                <Link
+                  to="/about"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tools"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  Tools
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/blogs"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         {/* Middle section with navigation links */}
-        <div className={`w-full md:flex md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-default">
+        <div className="hidden md:flex md:items-center md:space-x-4">
           <ul className="font-medium flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
@@ -78,7 +135,7 @@ const Navbar = () => {
           <REAButton />
         </div>
       </div>
-      
+
       {/* Horizontal line for mobile view */}
       <hr className="md:hidden border-t border-gray-300 dark:border-gray-300" />
     </nav>
