ab365e5386890977ee251266a0d5894df3deeec9
Keshav Gupta <guptakeshav0710@gmail.com>
2024-09-05 23:19:31 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,7 +30,7 @@ function Footer() {
     {
       title: "Tools",
       items: [
-        { name: "Budget Boss", path: "/tools/maintenance" },
+        { name: "Budget Boss", path: "/tools/maintainence" },
         { name: "Goal SIP", path: "/tools/goal-sip" },
         { name: "SIP Calculator", path: "/tools/sip" },
         { name: "F.I.R.E", path: "/tools/fire" },
