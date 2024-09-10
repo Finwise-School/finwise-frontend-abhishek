4f1d7d3d825716a1a487610f768d0f787f1f094c
AMANDEEPSINGHBHALLA <singhbhallaa@gmail.com>
2024-09-11 03:11:20 +0530

    TempChanges

--- src/components/Homepage/Footer Files/EmailSubscription.js
+++ src/components/Homepage/Footer Files/EmailSubscription.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import logo from '../../../assets/images/logo.png';
+import { Spinner, TextInput } from "flowbite-react";
+import axios from 'axios';
 
 function EmailSubscription() {
+  const [submit, isSubmit] = useState(false);
+  const [phone, setPhone] = useState();
+  // const onSubmit = () => {
+  //   if(phone) {
+  //     isSubmit(true);
+  //     console.log(phone);
+  //   }
+  // }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    isSubmit(true);
+
+    const phoneData = {
+      phone
+    }
+  
+    try {
+      const response = await axios.post('https://finwisebackend.onrender.com/api/phoneData', phoneData);
+
+      if (response.status === 201) { // Successful creation
+        console.log('Phone Number Received');
+        isSubmit(false);
+        // Reset form or navigate to another page
+        // resetForm();
+        // setIsSuccessful(true);
+      } else {
+        console.error('Error saving data:', response.data);
+        isSubmit(false);
+        // setIsSuccessful(false);
+      }
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-start min-h-[204px] w-full md:w-[538px]">
       <div className="flex items-center space-x-4">
@@ -10,14 +48,21 @@ function EmailSubscription() {
       </div>
       <form className="flex gap-2.5 items-center px-4 py-4 mt-6 w-full md:w-[80%] max-w-full text-lg font-medium rounded-xl border border-solid bg-neutral-900 border-neutral-800">
         <input
-          type="email"
-          id="emailInput"
-          placeholder="Enter Your Email"
+          type="number"
+          id="phoneInput"
+          placeholder="Enter Your Phone Number"
           className="flex-1 bg-transparent border-none outline-none text-white"
+          onChange={(e) => setPhone(e.target.value)}
+          required
         />
-        <button type="submit" aria-label="Submit email" className="bg-blue-500 px-3 py-2 rounded-md text-white">
-          Submit
-        </button>
+        {/* <TextInput id="phoneInput" type="number" onChange={(e) => setPhone(e.target.value)} placeholder="Enter Your Phone Number" className="flex-1 bg-transparent border-none outline-none text-white" required /> */}
+        {submit ? (
+                  <Spinner aria-label="Default status example" />
+        ) : (
+                  <button aria-label="Submit phone" className="bg-blue-500 px-3 py-2 rounded-md text-white" onClick={onSubmit}>
+                  Submit
+                </button>
+        )}
       </form>
     </div>
   );
