0ea3427d8c66b376c46cc0c2be073ad0519a8dce
AMANDEEPSINGHBHALLA <singhbhallaa@gmail.com>
2024-09-17 11:44:54 +0530

    Changes

--- src/components/Homepage/Header Files/Navbar.js
+++ src/components/Homepage/Header Files/Navbar.js
@@ -150,14 +150,14 @@ const Navbar = () => {
               </Link>
             </li>
             {/* Show this Link only To Admins */}
-            <li>
+            {/* <li>
               <Link
                 to="/admindashboard"
                 className={`block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${isActive('/blogs')}`}
               >
                 Admin
               </Link>
-            </li>
+            </li> */}
             
 
           </ul>
