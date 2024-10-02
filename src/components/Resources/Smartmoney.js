d6a08fd40d716edb08cde8ba3da8a2fd54ecbc0f
Shambhavi Rai <112109952+luna1902@users.noreply.github.com>
2024-10-02 21:49:37 +0530

    ui completely fixed

--- src/components/Resources/Smartmoney.js
+++ src/components/Resources/Smartmoney.js
@@ -48,25 +48,23 @@ const Smart = () => {
     <div className='p-[5%]'>
     <h1 className="finwise-blue text-5xl md:text-5xl font-bold mb-[8%] text-center">Smart with money</h1>
    
-    <div className='grid grid-cols-4 gap-[-10%] px-[8%]'>
-       
-        {images.map((image, index) => (
-            <div key={index} className="m-0 p-0">
-                <Link to={image.path}>
-                <img
-                    src={image.imageSrc}
-                    alt={`Book ${index}`}
-                    className="w-[100%] h-[100%] mt-[-20%] m-0"
-                />
-                </Link>
-                <h2 className='font-bold mt-[8%] mb-[4%] text-sm text-center  text-black'>{image.name}</h2>
-            </div>
-        ))}
-        
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-[8%]">
+      {images.map((image, index) => (
+        <div key={index} className="m-0 p-0">
+          <Link to={image.path}>
+            <img
+              src={image.imageSrc}
+              alt={`Book ${index}`}
+              className="w-full h-auto mt-0 m-0"
+            />
+          </Link>
+          <h2 className="font-bold mt-4 mb-2 text-sm text-center text-black">
+            {image.name}
+          </h2>
+        </div>
+      ))}
     </div>
-    
-</div>
-
+  </div>
   )
 }
 
