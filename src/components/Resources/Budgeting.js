8cd9ddde95ca17e4cdb5453a60f2d7efced9e05d
fwsuk <finwiseschool@gmail.com>
2025-06-15 22:41:46 +0530

    Update Budgeting.js

--- src/components/Resources/Budgeting.js
+++ src/components/Resources/Budgeting.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const images = [
   {
@@ -19,35 +18,19 @@ const Budgetings = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-[8%]">
         {images.map((image, index) => (
           <div key={index} className="m-0 p-0">
-            {image.path.startsWith("http") ? (
-              <a
-                href={image.path}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={image.imageSrc}
-                  alt={`Book ${index}`}
-                  className="w-full h-[80%] mt-0 m-0"
-                />
-              </a>
-            ) : (
-              <Link to={image.path}>
-                <img
-                  src={image.imageSrc}
-                  alt={`Book ${index}`}
-                  className="w-full h-[80%] mt-0 m-0"
-                />
-              </Link>
-            )}
+            <a
+              href={image.path}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={image.imageSrc}
+                alt={image.name}
+                className="w-full h-[80%] mt-0 m-0"
+              />
+            </a>
             <h2 className="font-bold mt-4 mb-2 text-sm text-center text-black">
               {image.name}
             </h2>
           </div>
         ))}
-      </div>
-    </div>
-  );
-};
-
-export default Budgetings;
